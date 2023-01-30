8322a79b017ee15671e26a5727ac9a90e391fbc6
bichomartiano <bichosiete@hotmail.com>
2023-01-30 16:18:46 +0100

    asv

--- another.py
+++ another.py
@@ -1,6 +1,5 @@
 from rpi_ws281x import *
 
-
 LED_COUNT = 10
 LED_PIN = 18
 LED_FREQ_HZ = 800000
@@ -9,3 +8,9 @@ LED_INVERT = False
 LED_BRIGHTNESS = 255
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+strip.begin()
+
+for x in range(0, LED_COUNT):
+    strip.setPixelColor(x, Color(0,0,255))
+
+strip.show()
