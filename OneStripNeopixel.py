9ec0038b4c32bb2764b39054f27d1ff791376551
bichomartiano <bichosiete@hotmail.com>
2023-01-30 16:05:10 +0100

    asdvdsv

--- OneStripNeopixel.py
+++ OneStripNeopixel.py
@@ -5,7 +5,7 @@ import neopixel
 
 #Initialise a strips variable, provide the GPIO Data Pin
 #utilised and the amount of LED Nodes on strip and brightness (0 to 1 value)
-pixels1 = neopixel.NeoPixel(board.D18, 30,bpp=4, brightness=1)
+pixels = neopixel.NeoPixel(board.D18, 30,bpp=4, brightness=1)
 
 #Also create an arbitary count variable
 x=0
@@ -14,11 +14,11 @@ x=0
 #based on decimal code R, G, B. Number can be anything from 255 - 0. Use a RGB Colour
 #Code Chart Website to quickly identify a desired fill colour.
 print("Filling red")
-pixels1.fill((255, 0, 0))
-
+pixels.fill((255, 0, 0))
+pixels.show()
 #Below demonstrates how to individual address a colour to a LED Node, in this case
 #LED Node 10 and colour Blue was selected
-pixels1[10] = (0, 20, 255)
+pixels[10] = (0, 20, 255)
 
 #Sleep for three seconds, You should now have all LEDs showing light with the first node
 #Showing a different colour
@@ -30,10 +30,10 @@ time.sleep(10)
 #which LED Node we will taget/index with a different colour
 
 #Below will loop until variabe x has value 35
-while x<35:
-    pixels1[x] = (255, 0, 0)
-    pixels1[x-5] = (255, 0, 100)
-    pixels1[x-10] = (0, 0, 255)
+while x<30:
+    pixels[x] = (255, 0, 0)
+    pixels[x-5] = (255, 0, 100)
+    pixels[x-10] = (0, 0, 255)
     #Add 1 to the counter
     x=x+1
     #Add a small time pause which will translate to 'smoothly' changing colour
@@ -41,9 +41,9 @@ while x<35:
 
 #below section is the same process as above loop just in reverse
 while x>-15:
-    pixels1[x] = (255, 0, 0)
-    pixels1[x+5] = (255, 0, 100)
-    pixels1[x+10] = (0, 255, 0)
+    pixels[x] = (255, 0, 0)
+    pixels[x+5] = (255, 0, 100)
+    pixels[x+10] = (0, 255, 0)
     x=x-1
     time.sleep(0.05)
 
@@ -51,6 +51,6 @@ while x>-15:
 time.sleep(4)
 
 #Complete the script by returning all the LED to off
-pixels1.fill((0, 0, 0))
+pixels.fill((0, 0, 0))
 
 print('Program finished!')
